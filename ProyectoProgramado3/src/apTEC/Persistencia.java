36d9db30afae700dbe7d3454ee2b2b2f07ec5a01
bara21sfj <bara200104@gmail.com>
2019-11-14 21:56:55 -0800

    Clase de persistencia

---
+++ ProyectoProgramado3/src/apTEC/Persistencia.java
@@ -0,0 +1,86 @@
+package apTEC;
+import java.io.*;
+
+public class Persistencia implements java.io.Serializable {
+	
+	/*static Persona persona1 = new Persona("Cesar",21,"306547");
+	
+	static String projectName = "Estadio";
+		
+	public static void serializeData() {
+		try {
+	        FileOutputStream fileOut = new FileOutputStream("C:\\Users\\Andres Barahona Coto\\Documents\\TEC\\II Semestre\\Algoritmos y Estructuras\\Proyectos\\PP3\\APTEC\\ProyectoProgramado3\\src\\apTEC\\" +
+	        projectName + ".txt");
+	        ObjectOutputStream out = new ObjectOutputStream(fileOut);
+	        out.writeObject(persona1);
+	        out.close();
+	        fileOut.close();
+	        System.out.printf("Serialized data is saved in /tmp/employee.ser");
+	     } catch (IOException i) {
+	        i.printStackTrace();
+	     }
+	}
+	
+	
+	public static void deserialzeData() {
+		try {
+	         FileInputStream fileIn = new FileInputStream("C:\\Users\\Andres Barahona Coto\\Documents\\TEC\\II Semestre\\Algoritmos y Estructuras\\Proyectos\\PP3\\APTEC\\ProyectoProgramado3\\src\\apTEC\\" +
+	     	 projectName + ".txt");
+	         ObjectInputStream in = new ObjectInputStream(fileIn);
+	         persona1 = (Persona) in.readObject();
+	         in.close();
+	         fileIn.close();
+			}
+	      catch (IOException i) {
+	         i.printStackTrace();
+	         return;
+	      	}
+	      catch (ClassNotFoundException c) {
+	         System.out.println("Person class not found");
+	         c.printStackTrace();
+	         return;
+	      	}	
+		
+		System.out.println("Deserialized person...");
+	    System.out.println("Name: " + persona1.name);
+	    System.out.println("Address: " + persona1.address);
+	    System.out.println("Number: " + persona1.number);
+	}
+	
+
+	public static void main(String[] args) {
+		//serializeData();
+		//deserialzeData();
+	}*/
+	
+		
+	
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
