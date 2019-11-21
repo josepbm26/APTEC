dba423d2917b3d8c9f7213094fbe443dbc5c9ba0
bara21sfj <bara200104@gmail.com>
2019-11-20 23:15:30 -0800

    Algoritmo de Dijkstra

--- ProyectoProgramado3/src/apTEC/Start.java
+++ ProyectoProgramado3/src/apTEC/Start.java
@@ -16,7 +16,7 @@ public class Start {
 	/**
 	 * Launch the application.
 	 */
-	public static void main(String[] args) {
+	public static void main(String[] args) {	
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
